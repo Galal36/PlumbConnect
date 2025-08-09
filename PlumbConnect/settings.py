2ec4f89641203b9842121b6ef4eaa95d21c3ec3f
Galal36 <galalmostafa362587@gmail.com>
2025-08-09 08:18:07 +0300

    Fixing some errors at integration phase

--- PlumbConnect/settings.py
+++ PlumbConnect/settings.py
@@ -136,7 +136,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME':'plum_connect',
         'USER':'postgres',
-        'PASSWORD':'Rrr@2021',
+        'PASSWORD':'123',
         'HOST': 'localhost',
         'PORT': '5432',
     }
